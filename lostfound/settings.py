508444765099f75d4c5ce03bd989284002f816b8
Sh1nX0 <116954589+Sh1nX0@users.noreply.github.com>
2026-01-23 12:39:04 +0000

    ///\

--- lostfound/settings.py
+++ lostfound/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'items',  # или 'items.apps.ItemsConfig'
 ]
 
 MIDDLEWARE = [
